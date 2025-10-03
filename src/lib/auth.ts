c2c610b28b85f4af4515849c33595652785985c9
Dy Rongrath <[118616647+Dy-Rongrath@users.noreply.github.com]>
2025-10-03 23:13:18 +0700

    fix: Update debug configuration to enable in development and disable in production

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -69,5 +69,5 @@ export const authOptions: NextAuthOptions = {
     },
   },
   secret: process.env.NEXTAUTH_SECRET,
-  debug: process.env.NODE_ENV === 'development',
+  debug: process.env.NODE_ENV !== 'production',
 };
